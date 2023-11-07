86dd20e2b76714644512ff1a73b83061c62fcfd5
ruthealmeida <ruthe.amaral@etec.sp.gov.br>
2023-11-07 17:04:13 -0300

    terminei a página sobre

--- carreira.php
+++ carreira.php
@@ -4,16 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="STYLES/config.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>MARINA - CARREIRA</title>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li>
-                <a>Sobre Ela</a>
-            </li>
-        </ul>
-    </nav>
     <div class="container">
         <div class="title">
         <h1>História da Marina</h1>
@@ -32,61 +26,9 @@
         Ela pertence àquele grupo de artistas com começos árduos que viram na música sua vocação e o meio de deixar sua marca.
         </div>
         <div class="imagem">
-            <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHCBUVFRgVFRUYGBgZGBgYFRgYGREaGRgYGBgaGRgaGhgcITAlHCErHxgYJ
-            jgmKzExNTU1GiQ7QDs0Py40NTEBDAwMEA8PGhESGDEhGB0xMTQxNDQ0PzQxMT8xMTExNDQxMTQ0PzE0MTE0PzExMTExMTExMTExMTExMTExMTExMf/AABEIAK4BIg
-            MBIgACEQEDEQH/xAAcAAEAAQUBAQAAAAAAAAAAAAAAAQIEBQYHAwj/xAA8EAACAQIEAwUFBgQHAQEAAAABAgADEQQSITEFBkEiUWFxgQcTMpGhQmKCscHwI1LR4RQ
-            zcpKiwvFjF//EABYBAQEBAAAAAAAAAAAAAAAAAAABAv/EABYRAQEBAAAAAAAAAAAAAAAAAAABEf/aAAwDAQACEQMRAD8A6BERASbyIgJN5Em0CYIiIACIiBF4MpZr
-            by2xfEKVJWao6Iqi7MzAAd2/U90C5MEgbzTuIe0HBrTZkqF22VVVgzH8QFh47Tn/ABnnrE4hWRctJGNzkLZ9rWL3t8gJcTXZMXxehSF6lRUHext/7NT4r7SMMlxSv
-            UbYaOov5kbTkFSqzG7sWPexY/nKLS4a6K3tPcnWlYdAjka95OW58gR6yj/9KcWsrMNc2YKDvoAczX8fynPgsMtoxNdIp+1FwwzUbp1IIVv9tyD8xNioe0fAtbMzoT
-            a4ZH0PdcCxnFhtF5MXX0Xw3jFHEJnpOHXqR0N9iDqJfKwOxnzXSqsvwuR32JE2bgPPGIw9lf8Aip3Me2o27LbEeB37xGGu32iYbl7mOji0ujDMB2kuMw9N5mQZFSD
-            KryiIFUkSBJECqJTECqIiAiIgIiICIiB52i0mIEESJVEAJVaQIgTaUyZEDHcb4zSwtM1KrWA2HVj0AHUmcl4r7QMRWYhC1NNQoUjOfEt08h9Z6+1biTPixRv2KSLY
-            dM7i7E+mUfPvmm0bHeWJWTxfMWJdcjV6uXuzsL+ZB18piSwN7kkne9ze21zK61K2tx5TxlCIiAkgSJ6KO+AQiL30vpKdSZ6Wt8oFBFtjKggIsND4ykm8ogVEFelpJ
-            YHpJp3Olrju3k1aJU7G0D2wGKek4em5VhqCDOs8tc+0qoC4h1pVNjmuEbxDfCL9xI+s43JU667de+MH0yrhgCCCDqCCCCPAiVTkXJfMj4VxSqXak2thro1mV07zYk
-            2G+29p12k6soZSCrAFSNiDqCJlVUkSJIgIEGBASqUxAqiIgIiICIiB5iTEkQIiTEAJVEQEplUpgcZ9qfDCmJNW2lSxB7+zY+ZBB+azRhPobmjgSYyg1JgL2ujaXRx
-            sR+XkZwLiGAqUHanVUq6mxBGh8Qeo8ZqM1bt5+UiTIhSIiBIEry9JQonve2sCFSw8Yqd0rl5wfhz4mqqU1La62BOnUk9PWBTwrgtXEOFRT4noJ0fg/srpOoaq737w
-            QB6TauVeUkoIC4BY6kaH9JtwFhYCTTGhJ7L8Io7LODprcH9/WTxDkCk6Bc5JXRSd7b2J6i97d1z5TenE8DGmOA848nPg+2pz0ybEgaqTtfwmpz6X4zg0q03puLq6l
-            WHgRPnDiOEajVek26MVPjbY+osZYlZbhn8Sg4Px0bFG7kYnQ+GawHdnPp1PkTihekqN1BK6g2cfGv6+jTlnKTn3pS/xoUtuCWZQl/xlT6Gb1yLUy1alPYJUzJv8DD
-            fy7TH8fhFI6NJEiJlpMCIEBESqAkGTFoCIiAiIgU2iVRARIEmAiAItARIMGBMw3GeXcPif81Ax2B6gdbGZmUsdIHHPaHywmHCPSTsktnIsAoOXJoOm4mhzffahxkV
-            Ki4dBpT7TNe92OwAtpa5v/aaFNIRECB6U0+m/hKgf7fp/WUk9PnBf9/rA9FNyB36f3na/ZzhQlEZEsCASbAZvG/Wc65J5XfE1M73WmupPU+AvO58MwqUkCIMoElSR
-            kUlc8MRi6dMXd0QfeZR+cxT834JTb36fX5+XjI0zTpPAiWuG5gw1W2SsjXvYBhc230MuhUBgW2JTScP9p3DymJFS2jrY/wCpf7EfKdzxVdVHaYKO9iAPmZyj2n4rD1q
-            YNOojujKSqkE5TofzliVoPBifeqRuAbd4JWwPjqR5bzo3JoBxla2zA5elgjsgt4EKPlOb4AWZG6XI169nXz+IfOdF9nal3z3GiOPHMrsbEnXTOh+UtSOljvi0RMtIlV
-            ovF4ESqUyqAiQJJgIiICIiAkCTAEAIkxAWkGTEBKRKrRAiWnFMclCk9Woewilm8QOg8SbD1l0TORe1LmM1H/wiEhEIar9591U+CjXzPhA0XieNavVes9szuWIFrC+w
-            HkLD0njRpM7Kii7MQFHiZRL3g9PNWRc+S5Pa100PcR0vNI2WnyOuQs2LphguZgquyr90nQ3v1HymnOhUkHcaabeYPWZbHYMIgOga5OaxzWN9S1/A2HrMS3jr6wAmQ4
-            Hw816yoATc9O7xmOm/+y7h2esXOgQX8yRbX5xR0rgHDVoUwii3fMnWQsLByg6lQM3oSNJdU6InljcAzoQjZSeukyrBVqGAw1zVCM7faqn3jt4ANc/KaTzNisBU0Sk
-            isdQ6tRS+9tM3h3CY/mvk3FK7OxeoDb+Jqw69kruBJ4JhMNRoOmIpPVqu2YKKRGQZbKoZ7b7322liVi8HwZ2OamSGvpc6j1nbuBYV0w6BmLELqTfUzVPZ3y0VRncOB
-            msiva+Ugb6nbb0nRKKWXLFHCeb+I18ViXSmWKU2KKFubkE3Nu8n6CYbFcFRKTOzk1ANEsQb9NCNZk+asJicNia1FQyo9UtTcNlUhzmBY9b3IJJtoe6WuGw1QYvLTvi
-            VpWqZWcHsqVBs3cGZdpYI4lw40Fw6NYOvvGYdzK7g38P4SfPym6+zCkMtRxsGKjQj4ggc67XKKbfd8Zr3N5YvTLAB2947L0W7/CTexHavNx9mlO2Hc23cW31GUHc+JM
-            VI3GJAEmZaUmBEQKolMqgItECBBkxEBERAmIiAiIgIiICIlLeMDA828wf4WkSgDVGIVA2igtpdj4am3h0vOC46sXdmLZyWLMx3dibs3qek6t7RwfctV7rU6a/y5zd20
-            +0wAGuwv3zkTCWM1TK6dQowdTYqQw8xqJTAEqsnxWoXsbkqWbLfqWJbNbvOaWAp3Nh+7by4QEgEn4RlXyubfn9J7UsLmUN6sDcXHW1ulreZPlcMcVnSvZnixTDL1bf0
-            uBNLqYMHQbggn5/kNZtXAcK1Mhh+/wB3ijr+GxQbYzJUXvNH4XjrGx/WbJhsX3TKss9MHeWj4NL3yLfvsJ6JiLz0WBKIANJKbzyxFcKN9zYes8/eBBmJ074Fhx3hdOq
-            R7xFYai5ANu79Zo78Dp4HErUTRKl6Ti42fUWv98J6XnRcRi6ZW+YWtvcTm/tH4iFooF+2bjvsPDyP1liVrXF6pxFc2W9gq76XK5wB5hFH4p0vlPC5MOvedT8gB+X1nM
-            +XcKatRQN76kX+NrE7fyhQd/st3WnZMNTCKFHQRSK4kGTIqkyJMQERKoCIiBFpMgCTAREQJiIgIiICJ5vVI+z9f6zyOLt9h/kmvldoFyTKSe6WzY5dyri3TKx+eW4ll
-            iOYKCXu503slQkegUwNH9qmKYBEv2Vuf9dRgbW8FW58ys5i6WAPft+v785tnM3ETj8WCpJRezTAVjlQfE2W1ySdfRZr2Pys5ykKo7KA5rhV0HTfqfGaiLCetID+pkui
-            jrfTpa2s8y3pA9hVuwsubUAKATfX4QOt+7reb1RwZLUjUTR2p3syZf4gdkV3vYXCt16WvqBNX4Vgmz06i5hZ1s23aVkDKDcXNnGlxvuN5u1Coy5nahWqOoFRV/w7j3/v
-            BmzuyOyvlAZQGynKxYHNqQxlRFfFugFhcEW2a4AFj1ANxcaG2k3PB4Ae7UWF1FvO9r/M6/KanzCz08VSqMHyrTVLNlBVM7lRlV2ygBhb4fLqd44Viw6qfARUjwfCFWu
-            NBMhg3Oxl+KQYSkYSxmWl7h2vL5G0lhQFpdBoFPEcCaqFVco1wyNa9mG1x1HQjxmjcX5Lr1Fd62Kd3AJRELLTFrkdi+57yZ0AVgBckADqSAB6ma9xTm/BpdDUznY5N
-            QPXr6QOecMwOKfECliWcIig5QTZlGysSdQevr3y19o2Iz4ijTBGiFvVmAUW81m3cV5lwlSonu3JcC2bKcp0+G/ft4TSuJD3/EVJBICoo278oIvvZ3/4mWJW4+z7hOV
-            UKkXNlJ3JB7bep0Hkf5pvMtuHYVaVNEXZVCj0H9bn1lw0UiDJiJFUmJJMiAlUplUBIEmIAxEQERECYiICIiB5N6ny/r0haXU/S35nUz0tJgUGkO76k/nNR564wKSDD
-            ICXq6EU758mxsALXb4de8npNrxmICIXPToNyegEwXCOCfxnxdbWq/wKdRTXZVXu07oGr0eAnA4OtWfL7107ehuhYgKit1sTrp1nNcaii1rG29j17ttbfrOx+0daj0E
-            oUhc1agUm+tlBfTwuouZjsLyGnuynxO6Wao1stMaEBFHUn6TWs45M1M5Qx2N7eY3/MSrAUA7hCbXD6+IRiPqBM3xbgTYer7pxZHzGm5AtsSVNuoZQp+ctOVmVcVSz2
-            ylipvsLgjfp/eFbly/w9WwSuRqHVwf5XDBHBOVwABY6owIaxBnrgqYVFCpiGuiM/8AgqjJTuuGZW95mYWqdoE5dLdTa02rhPDCMIgpgXeghszMtnCAZiy6i4te3cLW
-            3mp8aoshyOmMfJhqrBs4pIESkiBwqkB8oY5jYkqQCGsbQevCcBTrqiAUsnu8o9wxZASqE6lQwYMTe7Nr/LYg2eGxj4VzSqfZ+E/zL0MzfLLn3yUnNTOiJmSopR1BSmR
-            2FOQDcDKAxtdrnbMc18AWst7dobGNEcK46rjRhNhw9YML3nGqlOph362E2fgnM2wYxhro9xPDG4VqyFFqNTv9tMuYeVwReY/C8SVxo0yKVDaRWqY3ksNpXxuKdOgJUD
-            zJUH8hMTW5JwHSu/j20/VZ0CojsLAbzA8S5SFTttYnuP8A5CY0DHcu0MPUTJiHdQSzA5SQF1Go8RMjicEU4jgCwsKiItvvq/vX+tSXXCuCZ8Z7q1kTt1LbZRsv4jYeV
-            5kfaEChwuIFh7mvSN9BZGJWp6f5YlG7gSDKgZBkUiIgUmVRECmVREBIEmICIkWgTEi0mBMREBERAREQKKlMEgnpt4HvlQWTEDHYrC56yMdkV7fiK3/L6y/RbCVZZMDB
-            c0cMWrQfsgle2t+jLY308AQfOc64tyg1CqzL2kYZkIF8rkAjMP5bgi3cwnYSLgiUmktrW0l1MWHA7CgikEEIosbGwttec85hwoeriGWm7D3bjOlRsiu1RFzlfAqVdNs
-            yW8R0nH4lMPRepYWRCwG1yPhX1Nh6zmOMAYZ8rlneghqFERs2akxQ4dBYiwNtdSyknrEK9+UlA4g2QIEZcq+6d3p9lVvkd9W/YnU8VSzLNQ5dwOaq9RsxbOWzMqo2ot
-            qi6Ke8Dum9FLj0kVzvmfhF1JA/f7E0BsO6m6+s7VxXC3B0nN8TQCVXQ9dRLErC4PjD0ze5Hzm7cF5wSwDn9fpuJovFKQvpLzkvhorY2kp2p3qsO/JbKP8AcU9Ly1NdU
-            w/M9Ai+dR6y0x/NlK1kbOx0VV1LHuAGt5jufvdIlO3uEd2fV0XM6pTYlUfIQrXZbM1gDaZvl7hyU6FIimiP7tM7KASWKjN2yMx67zLSOAYF0RnqC1Sqc7jS6jZEv4C5
-            PixmL9otMHAVL9AQB4/F/wBBNrmne0Kpnp08Mmr1qii3XKND/wAnQfihK2ThVQtQpMd2poT5lQTLszzw1IIioNlUKPICw/KekKpMiSZMBERAREQEgSYgIkCDAXkxECY
-            iICIiAiIgIiIEiBAkwESBLPiuN9zTZ7XbZF/mc/CPLqfAGBqnOmLetVXCJqi5XrW6tuqnwAsfUd01/GG1dELBXz03YtqWBcKBfe9wbEmbny/wjQu/adzmdj1Y6mOKcp
-            VquIWslZEUKoCsHuHDqzMLaHsgi3efCWJjYOFYEIL98yZElFhpFWeLS4M5lzbTKOr+PznUK05/7QUvTLDoZYlaTxM6/WZz2W0c2Kqv0SiV9XdLfRDNWxVUn5C06H7MM
-            BkSu7DtFkQ/hUtb/nLUjN4xFrY1abEladHM9M06boxqs1szsptpT2FvPpM/LLB8PKVKtU1ajmoR2HK5ECiwFNQNPE9Z5cb4wmGQEgu7ELTppcu7nZVA/eky0r4vxNMP
-            TapUbKq6d7Mx+FVH2mJ2AmvctcPq1q7Y/ErlJFsNSJByJqAxI+0Qza/ePSwFxgOCVK7piMdlLqc1OgutOjfw1Dv943+VgNmAhCIiFQYkyICIiAiUxAqiIgIMCGgLyZE
-            QJiIgIiICIiAiSIMCRIkiBApeoFBZjYAEknYAbzW3qNiagYiyL8Cm/X7R8Tb0l3xuuWdaI0Wwd/va2VfLQk+ku8FhwAIF9gqYAAmRUTwoLLiBBMgmUuYBgW+M0E0fm1
-            M9F+8KT8pveIW6malxKnmzKeot8/8A2Bo/JvBvf1A7DsJr5npOj8uUAiVCBbNWc/IKn/WWXJODCUbfeYfI2/SZzh6WRh/9Kh+bmWpHu59PGYnh2Bz1DiaguxutEH7FM
-            7nwd7XPcLDob5WqLi0LoJFTIk2kEwEREBIMkxAiIiBTKoiAiIgSJBkyDAXkyIgf/9k=">
+            <img src="IMAGES-ICONS/marinanovo.jpg">                                                                       
         </div>
-
+    </div>
+    <a href="index.php"><div class="button-home"><i class='bx bxs-home' style='color:#135ec9'></i></div></a>
 </body>
 </html>
